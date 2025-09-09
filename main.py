ccde17363f5aea299668761ae33b5d33ca505ebf
ETweedie <ericttweedie131@gmail.com>
2025-09-08 23:00:06 -0400

    remove hard coded files and add file based look up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count, character_count, sort_dictionary
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,18 +8,22 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
-    print("============ BOOKBOT ==============")
-    print("Analyzing book found at books/frankenstein.txt")
-    print("------------ Word Count ------------")
-    print(f"Found {get_word_count(text)} total words ")
-    print("------------ Character Count -----------")
-    items = sort_dictionary(character_count(text))
-    for item in items:
-        if item["char"].isalpha():
-            print(f"{item["char"]}: {item["num"]}")
-        else:
-            continue
-    print("============ END ============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        text = get_book_text(sys.argv[1])
+        print("============ BOOKBOT ==============")
+        print(f"Analyzing book found at {sys.argv[1]}")
+        print("------------ Word Count ------------")
+        print(f"Found {get_word_count(text)} total words ")
+        print("------------ Character Count -----------")
+        items = sort_dictionary(character_count(text))
+        for item in items:
+            if item["char"].isalpha():
+                print(f"{item["char"]}: {item["num"]}")
+            else:
+                continue
+        print("============ END ============")
 
 main()
